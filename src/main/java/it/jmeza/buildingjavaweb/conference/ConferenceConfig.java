65819be2f7935347153efa04c26e8b69f42ad3f8
jmeza <jhonatan.meza01@gruppogaspari.it>
2025-03-10 21:27:46 +0100

    Fix/Creating Views with Spring MVC

---
+++ src/main/java/it/jmeza/buildingjavaweb/conference/ConferenceConfig.java
@@ -0,0 +1,26 @@
+package it.jmeza.buildingjavaweb.conference;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ConferenceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf/");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
