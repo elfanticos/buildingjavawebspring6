fe6238eb83acfc2036064b2f0d8524b129a01bcb
jmeza <jhonatan.meza01@gruppogaspari.it>
2025-03-13 21:59:36 +0100

    Fix/Creating a React Frontend with Spring MVC

---
+++ src/main/java/it/jmeza/buildingjavaweb/conference/model/User.java
@@ -0,0 +1,23 @@
+package it.jmeza.buildingjavaweb.conference.model;
+
+public class User {
+
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
